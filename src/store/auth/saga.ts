a272edb951b362bdd8d14af6b50d2cddc004e141
MichaelShipitsyn <michael.shipitsyn907@gmail.com>
2020-10-10 21:33:15 +0300

    Catch error check auth request

--- src/store/auth/saga.ts
+++ src/store/auth/saga.ts
@@ -21,11 +21,11 @@ function* loginRequest({ payload }: ReturnType<typeof loginRequestSaga>) {
     yield put(startLoader());
 
     const response = yield authApi.login(payload.email, payload.password);
-    yield LocalStorage.setItem('token', response.data.access_token);
+    LocalStorage.setItem('token', response.data.access_token);
     yield authApi.setHeaderAuthorization(response.data.access_token);
     const userData = yield authApi.getUser();
     yield put(setCurrentUser(userData.data));
-    yield LocalStorage.setItem('user', userData.data);
+    LocalStorage.setItem('user', userData.data);
 
     yield put(resetRequestError());
     yield put(stopLoader());
@@ -39,9 +39,13 @@ function* loginRequest({ payload }: ReturnType<typeof loginRequestSaga>) {
 function* checkAuthRequest() {
   const token = LocalStorage.getItem('token');
   yield authApi.setHeaderAuthorization(token);
-  const userData = yield authApi.getUser();
-  yield put(setCurrentUser(userData.data));
-  yield LocalStorage.setItem('user', userData.data);
+  try {
+    const userData = yield authApi.getUser();
+    yield put(setCurrentUser(userData.data));
+    LocalStorage.setItem('user', userData.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function* logoutRequest() {
