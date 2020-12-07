da30549abf841eaaea807b23337e148d7d687af3
MichaelShipitsyn <michael.shipitsyn907@gmail.com>
2020-12-08 01:50:29 +0300

    Remove extraReducers

--- src/store/employee/slice.ts
+++ src/store/employee/slice.ts
@@ -1,13 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User } from 'types/users';
 import { GetEmployeesResult } from 'api/employee';
-import { fetchEmployeesThunk, deleteEmployeesThunk } from './thunks';
 
 type InitialState = {
   employees: User[];
   totalEmployeesPages: number;
   isEmployeesFetchLoading: boolean;
-  error: unknown;
   currentPage: number | null;
   currentRowsPerPage: number | null;
 };
@@ -16,7 +14,6 @@ const initialState: InitialState = {
   employees: [],
   totalEmployeesPages: 0,
   isEmployeesFetchLoading: false,
-  error: null,
   currentPage: null,
   currentRowsPerPage: null
 };
@@ -24,27 +21,28 @@ const initialState: InitialState = {
 const employeeSlice = createSlice({
   name: 'employee',
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(fetchEmployeesThunk.pending, (state) => {
+  reducers: {
+    fetchEmployeesStart(state) {
       state.isEmployeesFetchLoading = true;
-      state.error = null;
-    });
-    builder.addCase(
-      fetchEmployeesThunk.fulfilled,
-      (state, { payload }: PayloadAction<GetEmployeesResult>) => {
-        state.employees = payload.employees;
-        state.totalEmployeesPages = payload.total;
-        state.isEmployeesFetchLoading = false;
-      }
-    );
-    builder.addCase(fetchEmployeesThunk.rejected, (state, action) => {
-      if (state.isEmployeesFetchLoading) {
-        state.isEmployeesFetchLoading = false;
-        state.error = action.payload;
-      }
-    });
+    },
+    fetchEmployeesSuccess(
+      state,
+      { payload }: PayloadAction<GetEmployeesResult>
+    ) {
+      state.employees = payload.employees;
+      state.totalEmployeesPages = payload.total;
+      state.isEmployeesFetchLoading = false;
+    },
+    fetchEmployeesFail(state) {
+      state.isEmployeesFetchLoading = false;
+    }
   }
 });
 
 export const employeeSliceReducer = employeeSlice.reducer;
+
+export const {
+  fetchEmployeesStart,
+  fetchEmployeesSuccess,
+  fetchEmployeesFail
+} = employeeSlice.actions;
--- src/store/employee/thunks.ts
+++ src/store/employee/thunks.ts
@@ -1,17 +1,29 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { employeeApi } from 'api/employee';
+import type { AppThunk } from 'store';
+import {
+  fetchEmployeesStart,
+  fetchEmployeesSuccess,
+  fetchEmployeesFail
+} from './slice';
 
 type FetchEmployeesParams = {
   page: number;
   limit: number;
 };
 
-export const fetchEmployeesThunk = createAsyncThunk(
-  'employee/fetchEmployees',
-  async ({ page, limit }: FetchEmployeesParams) => {
-    return await employeeApi.getEmployees({ page, limit });
+export const fetchEmployeesThunk = ({
+  page,
+  limit
+}: FetchEmployeesParams): AppThunk => async (dispatch) => {
+  try {
+    dispatch(fetchEmployeesStart());
+    const employees = await employeeApi.getEmployees({ page, limit });
+    dispatch(fetchEmployeesSuccess(employees));
+  } catch (err) {
+    dispatch(fetchEmployeesFail());
   }
-);
+};
 
 export const deleteEmployeesThunk = createAsyncThunk(
   'employee/deleteEmployees',
