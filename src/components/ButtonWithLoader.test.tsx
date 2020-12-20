87bbf36f46c304fe87498dfb8d1259b1d5ede70f
MichaelShipitsyn <michael.shipitsyn907@gmail.com>
2020-12-20 05:48:02 +0300

    Test coverage ButtonWithLoader

--- src/components/ButtonWithLoader.test.tsx
+++ src/components/ButtonWithLoader.test.tsx
@@ -3,8 +3,20 @@ import { render, screen } from '@testing-library/react';
 import * as React from 'react';
 import { ButtonWithLoader } from './ButtonWithLoader';
 
-test('contain label prop', async () => {
+test('check render with isLoading=false', async () => {
   render(<ButtonWithLoader label="test-label" isLoading={false} />);
 
+  expect(
+    screen.getByRole('button', { name: /test-label/i })
+  ).not.toBeDisabled();
   expect(screen.getByText('test-label')).toBeInTheDocument();
+  expect(screen.queryByRole('progressbar')).not.toBeInTheDocument();
+});
+
+test('check render with isLoading=true', async () => {
+  render(<ButtonWithLoader label="test-label" isLoading={true} />);
+
+  expect(screen.getByRole('button', { name: /test-label/i })).toBeDisabled();
+  expect(screen.getByText('test-label')).not.toBeVisible();
+  expect(screen.getByRole('progressbar')).toBeInTheDocument();
 });
